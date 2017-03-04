41eb23142ba56ebfdae31d258604a71ec47a81c4
DPK <DPK@DESKTOP-A2Q0OFC>
2017-03-04 14:32:45 -0500

    trimming the choices before printing them

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function askCleverbot(message){
 function choose(message) {
 	let listOfObjects = message.cleanContent.split(/choose/i)[1].split(/[,]|[\s]or[\s]/);
 	let numberOfObjectsToChooseFrom = listOfObjects.length;
-	message.reply('I choose ' + listOfObjects[getRandomInt(0, numberOfObjectsToChooseFrom)]);
+	message.reply('I choose ' + listOfObjects[getRandomInt(0, numberOfObjectsToChooseFrom)].trim());
 }
 
 function showMe(message) {
