0164319761b08b256572d107667f2bc6fe8aa032
DPK <DPK@DESKTOP-A2Q0OFC>
2017-03-04 14:28:44 -0500

    Update the choose split regex. before it was including the comma and an undefined

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function askCleverbot(message){
 }
 
 function choose(message) {
-	let listOfObjects = message.cleanContent.split(/choose/i)[1].split(/(\,)|( or )/);
+	let listOfObjects = message.cleanContent.split(/choose/i)[1].split(/[,]|[\s]or[\s]/);
 	let numberOfObjectsToChooseFrom = listOfObjects.length;
 	message.reply('I choose ' + listOfObjects[getRandomInt(0, numberOfObjectsToChooseFrom)]);
 }
