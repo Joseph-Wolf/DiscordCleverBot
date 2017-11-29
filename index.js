1a8c81ad5d23b13093c07c9a5581c55ffa896f67
jbone <jbone@metlife.com>
2017-11-29 17:01:32 -0500

    Fixing capitalization

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const Discordbot = require('./src/discordbot.js');
 const Cleverbot = require('./src/cleverbot.js');
 const init = require('./src/init.js');
 
-if(process !== null && process !== undefined && process.env !== null && process.env !== undefined && process.env.DBUrl !== null && process.env.DBUrl !== undefined){
-	MongoClient.connect(process.env.DBUrl, function(err, db){
+if(process !== null && process !== undefined && process.env !== null && process.env !== undefined && process.env.DBURL !== null && process.env.DBURL !== undefined){
+	MongoClient.connect(process.env.DBURL, function(err, db){
 	let collection = db.collection('mycollection');
 	init(collection, new Discordbot(), new Cleverbot(), config);
 	db.close();
