261e2cde57a48ea6e4bcabf2b2e9a84090baebf7
jbone <jbone@metlife.com>
2017-11-29 17:00:46 -0500

    Require passing DBURL as an environment variable.

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ const Discordbot = require('./src/discordbot.js');
 const Cleverbot = require('./src/cleverbot.js');
 const init = require('./src/init.js');
 
-let DBUrl = 'mongodb://localhost:27017/test';
-
 if(process !== null && process !== undefined && process.env !== null && process.env !== undefined && process.env.DBUrl !== null && process.env.DBUrl !== undefined){
-	DBUrl = process.env.DBUrl;
-}
-
-MongoClient.connect(DBUrl, function(err, db){
+	MongoClient.connect(process.env.DBUrl, function(err, db){
 	let collection = db.collection('mycollection');
 	init(collection, new Discordbot(), new Cleverbot(), config);
 	db.close();
 });
+}
