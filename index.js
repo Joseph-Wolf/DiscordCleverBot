32b5ace643a5b6071c8a83e5f2cae6d5826b5c8b
jbone <jbone@metlife.com>
2017-11-29 16:57:21 -0500

    Allowing users to pass Mongo key using an environment variable.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const Datastore = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const config = require('./config.json');
 const Discordbot = require('./src/discordbot.js');
 const Cleverbot = require('./src/cleverbot.js');
 const init = require('./src/init.js');
 
-Datastore.connect('mongodb://localhost:27017/myproject', function(err, db){
+let DBUrl = 'mongodb://localhost:27017/test';
+
+if(process !== null && process !== undefined && process.env !== null && process.env !== undefined && process.env.MongoURL !== null && process.env.MongoURL !== undefined){
+	DBUrl = process.env.MongoURL;
+}
+
+MongoClient.connect(DBUrl, function(err, db){
 	let collection = db.collection('mycollection');
 	init(collection, new Discordbot(), new Cleverbot(), config);
 	db.close();
