6a040448ff03f1a994a8d71354989b07bcb1d353
unknown <climber113691@gmail.com>
2017-11-29 18:36:57 -0500

    Removing test to test for no key found in the database for the cleverbot. It doesn't need to test that is the next test is testing a successful get from the db.

--- test/init/registerCleverbot.js
+++ test/init/registerCleverbot.js
@@ -7,22 +7,6 @@ const registerCleverbot = require('../../src/init/registerCleverbot.js');
 
 describe('registerCleverbot', function(){
 	before(testUtils.dbBefore);
-	it('should attempt to get key from db', function(done){
-		let mockCleverbot = {
-			DBKey: randomString(),
-			authenticate: function(value, callback){
-				assert.equal(value.user, null);
-				assert.equal(value.pass, null);
-				return done();
-			}
-		};
-		return testUtils.dbExecute(function(err, collection){
-			if(err){
-				return done(err);
-			}
-			return registerCleverbot(collection, mockCleverbot)
-		});
-	});
 	it('should get key from db', function(done){
 		let expectedPass = randomString();
 		let expectedUser = randomString();
