84a729b9897ae3d7fc44839b68787e4d06221142
unknown <climber113691@gmail.com>
2017-11-29 20:24:33 -0500

    but why

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 require('dotenv').config();
 
+const express = require('express');
+const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const config = require('./config.json');
 const Discordbot = require('./src/discordbot.js');
 const Cleverbot = require('./src/cleverbot.js');
 const init = require('./src/init.js');
 
-if(process !== null && process !== undefined && process.env !== null && process.env !== undefined && process.env.DBURL !== null && process.env.DBURL !== undefined){
-	MongoClient.connect(process.env.DBURL, function(err, db){
-		init(db.collection('mycollection'), new Discordbot(), new Cleverbot(), config);
-	});
-}
+MongoClient.connect(process.env.DBURL, function(err, db){
+	init(db.collection('mycollection'), new Discordbot(), new Cleverbot(), config);
+});
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
