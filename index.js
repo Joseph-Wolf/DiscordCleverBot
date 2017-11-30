ad6a196b440a780a207f3b4dc4b751830fc231d8
unknown <climber113691@gmail.com>
2017-11-29 19:48:07 -0500

    Adding a web server to make Heroku keep it running

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 require('dotenv').config();
 
 const MongoClient = require('mongodb').MongoClient;
+const express = require('express')
 const config = require('./config.json');
 const Discordbot = require('./src/discordbot.js');
 const Cleverbot = require('./src/cleverbot.js');
 const init = require('./src/init.js');
+const app = express()
 
-if(process !== null && process !== undefined && process.env !== null && process.env !== undefined && process.env.DBURL !== null && process.env.DBURL !== undefined){
-	MongoClient.connect(process.env.DBURL, function(err, db){
-		init(db.collection('mycollection'), new Discordbot(), new Cleverbot(), config);
-	});
-}
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+	if(process !== null && process !== undefined && process.env !== null && process.env !== undefined && process.env.DBURL !== null && process.env.DBURL !== undefined){
+		MongoClient.connect(process.env.DBURL, function(err, db){
+			init(db.collection('mycollection'), new Discordbot(), new Cleverbot(), config);
+		});
+	}
+});
