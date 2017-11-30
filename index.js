422bfb4117af1c1536ff023566940ae9110ab33e
unknown <climber113691@gmail.com>
2017-11-29 19:58:58 -0500

    why you no do dis?

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const Cleverbot = require('./src/cleverbot.js');
 const init = require('./src/init.js');
 const app = express()
 
+if(process !== null && process !== undefined && process.env !== null && process.env !== undefined && process.env.DBURL !== null && process.env.DBURL !== undefined){
+	MongoClient.connect(process.env.DBURL, function(err, db){
+		init(db.collection('mycollection'), new Discordbot(), new Cleverbot(), config);
+	});
+}
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
@@ -17,9 +23,4 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
-	if(process !== null && process !== undefined && process.env !== null && process.env !== undefined && process.env.DBURL !== null && process.env.DBURL !== undefined){
-		MongoClient.connect(process.env.DBURL, function(err, db){
-			init(db.collection('mycollection'), new Discordbot(), new Cleverbot(), config);
-		});
-	}
 });
