60b39ad1a153fa7cc89c64bc3e379d2b45fdb896
jbone <jbone@metlife.com>
2017-12-01 09:55:55 -0500

    Reorganizing getFact.js and showMe.js

--- src/util/getFact.js
+++ src/util/getFact.js
@@ -4,50 +4,82 @@ const catFact = require('cat-facts');
 const dogFacts = require('dog-facts');
 const request = require('request');
 
-module.exports = function(subject, callback) { 
-	let number = parseInt(subject.match(/[\d]+/));
-	let url = 'http://numbersapi.com/';
-	if (!Number.isInteger(number)) {
-		number = 'random'
+function parseNumbers(text){
+	let matches = text.match(/[\d]+/g);
+	if(matches === null || matches === undefined){
+		return [];
 	}
+	return matches.map(function(number){return parseInt(number)});
+}
+
+function callURL(url, callback){
+	if(url === null || url === undefined){
+		return;
+	}
+	return request({url:url}, function(err, response, body){
+		if(err){
+			console.error(err);
+			return callback(err);
+		}
+		if(response.statusCode === 200){
+			return callback(null, body);
+		}
+		console.error('StatusCode: ' + response.statusCode);
+		return callback('StatusCode: ' + response.statusCode);
+	});
+}
+
+function yearFacts(year, callback){
+	if(year === null || year === undefined){
+		year = 'random';
+	}
+	return callURL('http://numbersapi.com/' + year + '/year', callback);
+}
+
+function dateFacts(month, day, callback){
+	let suffix = month + '/' + day;
+	if(month === null || month === undefined || day === null || day === undefined || month > 12 || day > 32){
+		suffix = 'random';
+	}
+	return callURL('http://numbersapi.com/' + suffix + '/date', callback);
+}
+
+function triviaFacts(number, callback){
+	if(number === null || number === undefined){
+		number = 'random';
+	}
+	return callURL('http://numbersapi.com/' + number + '/trivia', callback);
+}
+
+function mathFacts(number, callback){
+	if(number === null || number === undefined){
+		number = 'random';
+	}
+	return callURL('http://numbersapi.com/' + number + '/math', callback);
+}
+
+module.exports = function(subject, callback) { 
+	let numbers = parseNumbers(subject);
 	try {
 		if(/cat/i.test(subject)) {
 			return callback(null, catFact.random());
 		} 
-		else if(/dog/i.test(subject)) {
+		if(/dog/i.test(subject)) {
 			return callback(null, dogFacts.random());
-		} else if (/year/i.test(subject)) {
-			url = url + number + '/year';
-		} else if (/date/i.test(subject)) {
-			let matches = subject.match(/[\d]+/g);
-			let numberTwo;
-			if(matches !== null && matches.length >= 2){
-				numberTwo = parseInt(matches[1]);
-			}
-			if(number === 'random' || numberTwo === undefined || number > 12 || numberTwo > 32) { //try to make sure the date is simi accurate
-				url = url + 'random';
-			} else {
-				url = url + number + '/' + numberTwo;
-			}
-			url = url + '/date';
-		} else if (/trivia/i.test(subject)) {
-			url = url + number + '/trivia';
-		} else if (/number/i.test(subject)) {
-			url = url + number + '/math';
-		} else {
-			return callback(null, 'I have facts about cats, dogs, years, dates (month/day), numbers, and number trivia.');
 		}
-		request({ url: url }, function (err, response, body) {
-			if(err){
-				console.error(err);
-				return callback(err);
-			}
-			if(response.statusCode === 200){
-				return callback(null, body);
-			}
-			console.error('StatusCode: ' + response.statusCode);
-			return callback('StatusCode: ' + response.statusCode);
-		});
+		if (/year/i.test(subject)) {
+			return yearFacts(numbers[0], callback);
+		}
+		if (/date/i.test(subject)) {
+			return dateFacts(numbers[0], numbers[1], callback);
+		}
+		if (/trivia/i.test(subject)) {
+			return triviaFacts(numbers[0], callback);
+		}
+		if (/number/i.test(subject)) {
+			return mathFacts(numbers[0], callback);
+		}
+		return callback(null, 'I have facts about cats, dogs, years, dates (month/day), numbers, and number trivia.');
 	} catch (e) {
 		console.error(e.message);
 		return callback('I encountered an error getting the fact for you.');
--- src/util/showMe.js
+++ src/util/showMe.js
@@ -1,16 +1,17 @@
 "use strict";
 
-const randomInt = require('random-int');
+const randomItem = require('random-item');
+const validSizes = [240, 250, 300, 350, 400, 450, 500, 550, 600];
+const validCategories = ['abstract', 'animals', 'cats', 'city', 'food', 'nature', 'dogs']
 
-module.exports = function(desiredCategory, callback) {
-	let randomSizes = [240, 250, 300, 350, 400, 450, 500, 550, 600];
-	let availableCategories = ['abstract', 'animals', 'cats', 'city', 'food', 'nature', 'dogs'];
-	let width = randomSizes[randomInt(randomSizes.length)];
-	let height = randomSizes[randomInt(randomSizes.length)];
-	let urlParts = [width, height, desiredCategory];
+function isValidCategory(category){
+	return validCategories.indexOf(category.toLowerCase()) !== -1;
+}
 
-	if(availableCategories.includes(desiredCategory.toLowerCase())) {
+module.exports = function(category, callback) {
+	if(isValidCategory(category)) {
+		let urlParts = [randomItem(validSizes), randomItem(validSizes), category];
 		return callback(null, 'http://loremflickr.com/' + urlParts.join('/'));
 	}
-	return callback('No. I only have pictures of ' + availableCategories.join(', '));
+	return callback('No. I only have pictures of ' + validCategories.join(', '));
 }
